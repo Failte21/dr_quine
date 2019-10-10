53e55f2eb63f6cfc4c7761e574901a51b3aa5e2d
Leo SIMON <lsimon@e2r8p15.42.fr>
2019-10-10 14:27:13 +0200

    initial_commit

---
+++ C/Colleen.c
@@ -0,0 +1,15 @@
+/*
+	This program will print its own source when run.
+*/
+
+void	other_fn()
+{
+
+}
+
+int		main()
+{
+	/*
+		This program will print its own source when run.
+	*/
+}
---
+++ C/Grace.c
@@ -0,0 +1,8 @@
+# define FT(x)int main(){ /* code */ }
+# define B
+# define C
+
+/*
+	This program will print its own source when run.
+*/
+FT(1)
